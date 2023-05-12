1843392291cfd0bc9dc71958e1e4f3d086fa800d
Kdorina <kovacsdorina2@gmail.com>
2023-05-12 17:37:54 +0200

    section 4 kész

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  ngOnInit(): void {
+    this.timeLine();
+  }
+
+  timeLine(){
+    let observer = new IntersectionObserver((entries) =>{
+      entries.forEach((entry) => {
+        if(entry.isIntersecting){
+          entry.target.classList.add('show-me');
+        }else{
+          entry.target.classList.remove('show-me');
+        }
+      })
+    })
+    let hiddenElements = document.querySelectorAll('.section');
+    hiddenElements.forEach((el)=> observer.observe(el));
+  }
 
 }
