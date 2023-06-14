32827dadc2bdb0690732286ea12ed555dd16f063
Kdorina <kovacsdorina2@gmail.com>
2023-06-14 16:46:49 +0200

    service metodusok megirasa reg / log / logout folyamathoz

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,48 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  host = 'http://localhost:8000/api';
+  host = 'http://localhost:8000/api/';
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  login(data:any){
+
+    let header = new HttpHeaders(
+    {
+      'Content-Type':'application/json',
+      'Accept':'application/json'
+    });
+    let httpOptions ={
+      headers: header
+    }
+    let url = this.host+'login'
+
+    return this.http.post<any>(url, data, httpOptions);
+  }
+  register(data:any){
+    let header = new HttpHeaders(
+      {
+        'Content-Type':'application/json',
+        'Accept':'application/json'
+      });
+      let httpOptions ={
+        headers: header
+      }
+      let url = this.host+'register'
+
+      return this.http.post<any>(url, data, httpOptions);
+  }
+  logout(data:any, token:string){
+    let header = new HttpHeaders(
+      {
+        'Content-Type':'application/json',
+        'Authorization':'Bearer'+token
+      });
+      let httpOptions ={
+        headers: header
+      }
+      let url = this.host+'logout'
+
+      return this.http.post<any>(url, data, httpOptions);
+  }
 }
