74c53e63514fe61e2a274f6efae39398fba1e0b3
Kdorina <kovacsdorina2@gmail.com>
2023-06-15 10:35:59 +0200

    login metódus megirása kész

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  loginForm !: FormGroup;
+
+  constructor( private api:AuthService , private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email:[''],
+      password:['']
+    })
+  }
+
+  login(){
+    let data ={
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    }
+    this.api.login(data).subscribe({
+      next:data=>{
+        console.log('sikeres bejelentkezes')
+      }
+    })
+  }
 }
